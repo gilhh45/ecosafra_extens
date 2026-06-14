bf02bcef5dcfa4c8dec97a53e15a2ff63f18abf7
Gilvan Ferreira <gottimeira015@gmail.com>
2026-06-14 16:56:29 -0400

    Django, a partir de agora, quando o usuário acessar a página inicial do sistema (''), não mostre mais um HTML estático puro. Chame o nosso garçom inteligente (client_list), faça ele buscar todos os clientes no banco de dados e monte a tela com informações reais em tempo de execução.

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+# Importamos as views do seu app clients
+from clients import views as client_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    
+    
+    path('', client_views.client_list, name='home'),
 ]
